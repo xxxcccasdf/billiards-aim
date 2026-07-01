6478c3e2fd6831ce4245521db1e4c6bf5c42acdd
TermuxFix <termux@fix.com>
2026-07-02 03:03:06 +0800

    fix: Android 16 - use native Activity, compileSdk 35, remove AppCompat

--- app/src/main/java/com/billiards/aim/MainActivity.java
+++ app/src/main/java/com/billiards/aim/MainActivity.java
@@ -1,10 +1,10 @@
 package com.billiards.aim;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
